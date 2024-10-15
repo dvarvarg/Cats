8855c4a80e3ffd40c7e7d05ac119231c03a37ce4
dvarvarg <dvarvarg@gmail.com>
2024-10-15 12:28:07 +0300

    Создана функция для получения изображения с котиком

--- Caats.py
+++ Caats.py
@@ -4,6 +4,18 @@ import requests
 from io import BytesIO
 
 
+def load_image():
+    try:
+        response=requests.get(url) #response-ответ, requests-запрос по ссылке, а то, что вернется, положим в response
+        response.raise_for_status() #для обработки исключений
+        image_data=BytesIO(response.content)
+        img=Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f'Произошла ошибка: {e}')
+        return None
+
+
 window=Tk()
 window.title('Cats!')
 window.geometry('600x480')
