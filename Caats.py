4e0b98a9c780a0425fdba8946a8a2431a06abc88
dvarvarg <dvarvarg@gmail.com>
2024-10-15 13:09:32 +0300

    Теперь каждое изображение открывается в новом окне

--- Caats.py
+++ Caats.py
@@ -18,10 +18,14 @@ def load_image(url):
 
 
 
-def set_image():
+def open_new_window():
     img = load_image(url)
     if img:
-        label.config(image=img)
+        new_window=Toplevel()
+        new_window.title('Картинка с котиком')
+        new_window.geometry('600x480')
+        label = Label(new_window,image=img)
+        label.pack()
         label.image = img  # чтобы сборщик мусора картинку не убрал
 
 
@@ -33,18 +37,17 @@ window=Tk()
 window.title('Cats!')
 window.geometry('600x520')
 
-label=Label()
-label.pack()
 
-update_button=Button(text='Обновить',command=set_image)
-update_button.pack(side=BOTTOM)
+
+# update_button=Button(text='Обновить',command=open_new_window)
+# update_button.pack(side=BOTTOM)
 
 menu_bar=Menu(window)
 window.config(menu=menu_bar)
 
 file_menu=Menu(menu_bar,tearoff=0)
 menu_bar.add_cascade(label='Файл',menu=file_menu)
-file_menu.add_command(label='Загрузить фото',command=set_image)
+file_menu.add_command(label='Загрузить фото',command=open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label='Выход',command=exit)
 
